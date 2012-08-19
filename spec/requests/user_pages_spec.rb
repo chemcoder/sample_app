2306e10017604ed5de6f179aa77cea84d2dfbffe
Vijayendra <vijayendra.c.o.o.l@gmail.com>
2012-08-19 14:37:30 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,9 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        it {should have_link('Sign in')}
+      end
     end
   end
 end
